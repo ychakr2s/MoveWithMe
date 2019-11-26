4b881230fd644207ecf72a04035b137f670cda98
Yassine <yassine.chakri.gus@gmail.com>
2019-11-27 00:35:38 +0100

    first Commit with Springboot

---
+++ src/main/java/ma/YassineGroup/fahrenApplication.java
@@ -0,0 +1,12 @@
+package ma.YassineGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class fahrenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(fahrenApplication.class, args);
+    }
+}
